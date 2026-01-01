8a477fe636bf01e3c1e303103fca3113a8c457ae
Berserker268 <mahdirahman268@gmail.com>
2026-01-01 23:33:26 +0600

    Updated

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ const mysql = require('mysql2');
  * - DB_NAME (default: 'onlynotes-db')
  */
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || 'admin',
-    database: process.env.DB_NAME || 'onlynotes-db' 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
