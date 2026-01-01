79c78c180862e97b8fea73a16075f24274c1786c
Ilham Morsalin <ilhammorsalin@gmail.com>
2026-01-01 22:41:30 +0600

    migrated from xaamp to mysql workbench

--- server.js
+++ server.js
@@ -14,10 +14,18 @@ const multer = require('multer')
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',      
-    password: '',      
-    database: 'onlynotes'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'onlynotes'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
 });
 
 const initializePassport = require('./passport-config')
